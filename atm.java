c9ae39d30bfbee2d0c16d4a8177ec1ac610384a1
Dakshanamurthy <113338215+Dakshanamurthy@users.noreply.github.com>
2023-04-29 10:34:21 +0530

    Add files via upload

---
+++ atm.java
@@ -0,0 +1,19 @@
+import java.io.*;
+im[ort java.util.*;
+class ATM
+{
+	public static void main(String args[])
+	{
+		inta,b,c,d,total;
+		system.out.println("no of 2000 notes");
+		a=5;
+		system.out.println("no of 500  notes");
+		b=4;
+		system.out.println("no of 200  notes");
+		c=3;
+		system.out.println("no of 100  notes");
+		d=5;
+		total=2000*a+500*b+200*c+100*d;
+		system.out.println("Total Amount"+total);
+	}
+}
